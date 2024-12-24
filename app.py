818952c51177a578588111afab7ea2b2edbe39b8
bruunosanttos <bruno-vieira-santos@hotmail.com>
2024-12-24 19:03:16 -0300

    feat:adicionando banco de dados sql alchemy

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import banco #colocando aqui pra ser executado somente se for chamado o APP.py por isso é chamado so aqui.
+    banco.init_app(app)
     app.run(debug=True)
 
     #http://127.0.0.1:5000/hoteis
