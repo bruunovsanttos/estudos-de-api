a307a272f275b0510bc36376091e41130ce0431b
bruunosanttos <bruno-vieira-santos@hotmail.com>
2024-12-30 20:52:42 -0300

    feat: criando o metodo update_hotel no models\hotel

--- models/hotel.py
+++ models/hotel.py
@@ -38,6 +38,8 @@ class HotelModel(banco.Model):
     def save_hotel(self):
         banco.session.add(self)# adiciona o objeto ao banco de dados
         banco.session.commit()
-    def update_hotel(self):
-        pass
-    
+    def update_hotel(self, nome, estrelas, diaria, cidade):
+        self.nome = nome
+        self.estrelas = estrelas
+        self.diaria = diaria
+        self.cidade = cidade
