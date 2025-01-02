8c198366d0768f6081f97659259343d27c61b96e
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-01-02 12:58:29 -0300

    feat: add resourfce de usuario no app.py e importando o modelo da classe

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
+from resources.usuario import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db' #cria um banco na raiz do programa
@@ -13,6 +14,7 @@ api = Api(app)
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(User, 'usuarios/<int:user_id>')
 
 if __name__ == '__main__':
     from sql_alchemy import banco #colocando aqui pra ser executado somente se for chamado o APP.py por isso é chamado so aqui.
