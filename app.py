1afefa014173058f4712d13ef799cf01c5371fca
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-01-07 13:01:06 -0300

    refactor: a biblioteca de jwt alterour e foi necessario fazzerr a alteração no programa

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 from resources.usuario import User, UserRegister, UserLogin, UserLogout
 from flask_jwt_extended import JWTManager
-import json
 from blacklist import BLACKLIST
 
 app = Flask(__name__)
@@ -28,17 +27,28 @@ api.add_resource(UserRegister, '/cadastro')
 api.add_resource(UserLogin,'/login')
 api.add_resource(UserLogout,'/logout')
 
+
+@jwt.token_in_blocklist_loader
+def verifica_blacklist(jwt_header, jwt_payload):
+    # Verifica se o 'jti' do token está na blacklist
+    return jwt_payload['jti'] in BLACKLIST
+
+# Função para tratar tokens revogados
+@jwt.revoked_token_loader
+def token_de_acesso_invalidado(jwt_header, jwt_payload):
+    return jsonify({'message': 'You have been logged out'}), 401  # Retorna o erro 401 se o token foi revogado
+
 if __name__ == '__main__':
     from sql_alchemy import banco #colocando aqui pra ser executado somente se for chamado o APP.py por isso é chamado so aqui.
     banco.init_app(app)
     with app.app_context():
         banco.create_all()
-    @jwt.token_in_blocklist_loader()
-    def verifica_blacklist(self, token):
-        return token['jti'] in BLACKLIST
-    @jwt.revoked_token_loader()
-    def token_de_acesso_invalidado(jwt_header, jwt_payload):
-        return json({'message': 'You have been logged out'}), 401#unatorized
+    #@jwt.token_in_blocklist_loader()
+    #def verifica_blacklist(self, token):
+    #    return token['jti'] in BLACKLIST
+    #@jwt.revoked_token_loader()
+    #def token_de_acesso_invalidado(jwt_header, jwt_payload):
+    #    return json({'message': 'You have been logged out'}), 401#unatorized
 
 
 
